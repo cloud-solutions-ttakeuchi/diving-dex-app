e683eac292174fae79775f86db18ad0676740c3f
cloud-solutions-ttakeuchi <t.takeuchi.a@gmail.com>
2025-12-09 22:32:18 +0900

    error修正

--- src/pages/EditCreaturePage.tsx
+++ src/pages/EditCreaturePage.tsx
@@ -47,6 +47,7 @@ export const EditCreaturePage = () => {
   // Load data into form when creature is found
   useEffect(() => {
     if (existingCreature) {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
       setFormData({
         name: existingCreature.name,
         scientificName: existingCreature.scientificName || '',
--- src/pages/EditPointPage.tsx
+++ src/pages/EditPointPage.tsx
@@ -31,7 +31,7 @@ export const EditPointPage = () => {
 
   useEffect(() => {
     if (existingPoint) {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
+
       setFormData({
         name: existingPoint.name,
         region: existingPoint.region,
